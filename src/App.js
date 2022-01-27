3cc99b1233e07c1b6fd880918f7b4188d4a5003f
Gabriel <gmamanican@unsa.edu.pe>
2022-01-27 12:06:46 -0500

    LastChanges

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const getItems = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/photos"
+    ).then((response) => response.json());
+    for (let i = 0; i < 20; i++) {
+      setItems((oldItems) => [...oldItems, response[i]]);
+    }
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {items.map((item) => (
+          <Grid item xs={2} sm={11} md={2} key={item.id}>
+            <Card sx={{ minWidth: 150, maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={item.thumbnailUrl}
+                alt={item.title}
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {item.title}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
